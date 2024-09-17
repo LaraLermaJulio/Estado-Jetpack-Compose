08b8a3e64d30cb5becffb61df24004ad673f876c
Julio Lara <s21120175@alumnos.itsur.edu.mx>
2024-09-16 20:30:08 -0600

    base

--- app/src/main/java/com/example/estadoenjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/estadoenjetpackcompose/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.example.estadoenjetpackcompose
 
+import WellnessScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.estadoenjetpackcompose.ui.theme.EstadoEnJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            EstadoEnJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            BasicStateCodelabTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
                 }
             }
         }
@@ -31,17 +31,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
 fun GreetingPreview() {
-    EstadoEnJetpackComposeTheme {
-        Greeting("Android")
+    BasicStateCodelabTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            WellnessScreen()
+        }
     }
 }
